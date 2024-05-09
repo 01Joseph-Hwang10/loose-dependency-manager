ec6fa46a0d7908ae085abf4f2f3d2cb8f4f62599
01Joseph-Hwang10 <joseph95501@gmail.com>
2024-05-10 02:44:47 +0900

    fix: ci script

--- ldm/component.py
+++ ldm/component.py
@@ -3,6 +3,5 @@ from ldm.logger import Logger, NoopLogger
 
 
 class Component(metaclass=abc.ABCMeta):
-
     def __init__(self, logger: Logger | None = None) -> None:
         self.logger = logger or NoopLogger()
