d42e255775dbc2a688a0e53fdec2e6638f0c4559
01Joseph-Hwang10 <joseph95501@gmail.com>
2024-05-16 04:24:49 +0900

    feat: more verbose logging

--- ldm/api/installer/strategy/_sequential.py
+++ ldm/api/installer/strategy/_sequential.py
@@ -6,5 +6,6 @@ class SequentialInstallStrategy(InstallStrategy[SequentialConfig]):
     def install(self, dependencies):
         self.logger.debug("Installing dependencies sequentially")
         for dependency in dependencies:
+            self.logger.debug(f"Installing: {dependency.name}")
             dependency.install()
-            self.logger.info(f"Installed {dependency.name}")
+            self.logger.info(f"Installed: {dependency.name}")
