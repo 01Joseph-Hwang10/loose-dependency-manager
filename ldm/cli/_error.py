9e43fa55e46031a506f8d8d3e4379b18afbbe96f
01Joseph-Hwang10 <joseph95501@gmail.com>
2024-05-16 04:23:26 +0900

    feat: add debug log in error

--- ldm/cli/_error.py
+++ ldm/cli/_error.py
@@ -1,4 +1,5 @@
 from typing import Callable
+from traceback import format_exc
 from ..logger import Logger
 
 
@@ -6,6 +7,8 @@ def error_boundary(func: Callable, logger: Logger):
     try:
         func()
     except Exception as e:
+        for line in format_exc().split("\n"):
+            logger.debug(line)
         for line in str(e).split("\n"):
             logger.error(line)
         exit(1)
